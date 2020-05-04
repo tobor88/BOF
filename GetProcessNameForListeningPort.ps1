8b74a883dba970a1e7f31fb2ae4ef25158901eaa
tobor <rosborne@osbornepro.com>
2020-05-04 13:58:32 -0600

    Update GetProcessNameForListeningPort.ps1

--- GetProcessNameForListeningPort.ps1
+++ GetProcessNameForListeningPort.ps1
@@ -3,4 +3,4 @@
 $ListeningPort = Read-Host "What port is the process listening on? Example: 80"
 
 $ProcessID = Get-NetTCPConnection -State Listen | Where-Object -Property localport -like $ListeningPort | Select-Object -Property OwningProcess
-Get-Process | Where-Object -Property Id -like $ProcessID.OwningProcess | Select-Object -Property ProcessName
+Get-Process | Where-Object -Property Id -like $ProcessID.OwningProcess | Select-Object -Property ProcessName,Id
